ef413eec845549b8f5c38b44ded22576cd57750f
Tim <115091219+JustUselessCode@users.noreply.github.com>
2024-03-21 13:18:59 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,15 +5,13 @@ namespace IpAddressAnalyzer
 {
     internal class Program
     {
-        
-
-
-
         static void Main(string[] args)
         {
             var IpAddress = "192.168.10.183";
-            var SubnetMask = "255.255.255.248";
+            var SubnetMask = "255.255.255.240";
+
             byte[][] arr = Formater.SplitAndConvert(IpAddress, SubnetMask);
+
             Address adr = new();
             adr.FirstOctet = arr[0][0];
             adr.SecondOctet = arr[0][1];
@@ -21,10 +19,10 @@ namespace IpAddressAnalyzer
             adr.FourthOctet = arr[0][3];
             adr.SubnetMask = SubnetMask;
             adr.NetworkAddress = Formater.GetNetworkAddress(arr[0], arr[1]);
+            adr.BroadcastAddress = Formater.GetBroadCastAddress(arr[0], arr[1]);
+            adr.TotalPossibleHosts = Formater.GetSubnettingHostAmount(arr[0], arr[1]);
 
-
-            Console.WriteLine("Network Address: " + Formater.GetNetworkAddress(arr[0], arr[1]));
-            Console.WriteLine("Broadcast Address: " + Formater.GetBroadCastAddress(arr[0], arr[1]));
+            Console.WriteLine(adr.ToString());
 
         }
     }
