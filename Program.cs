15b68ae4b1721c820428c987858085ffc26a0cfb
Tim.Zander <tim.zander@edu.siemens-energy.com>
2024-03-27 21:53:20 +0100

    asdasd

--- Program.cs
+++ Program.cs
@@ -36,36 +36,6 @@ namespace IpAddressAnalyzer
 
                 Console.WriteLine("\n" + ctx.ToString());
             }
-
-            // FOR TESTING AND DEMONSTARTION PURPOSES ONLY
-
-            //var IpAddress = "192.168.10.183";
-            //var SubnetMask = "255.255.255.240";
-            //var SubnetMask2 = "255.255.240.0";
-
-            //byte[][] arr = Formater.SplitAndConvert(IpAddress, SubnetMask);
-            //byte[][] arr2 = Formater.SplitAndConvert(IpAddress, SubnetMask2);
-
-            //AddressContext adr = new();
-            //adr.FirstOctet = arr[0][0];
-            //adr.SecondOctet = arr[0][1];
-            //adr.ThirdOctet = arr[0][2];
-            //adr.FourthOctet = arr[0][3];
-            //adr.SubnetMask = SubnetMask;
-            //adr.NetworkAddress = Formater.GetNetworkAddress(arr[0], arr[1]);
-            //adr.BroadcastAddress = Formater.GetBroadCastAddress(arr[0], arr[1]);
-            //adr.TotalPossibleHosts = Formater.GetSubnettingHostAmount(arr[0], arr[1]);
-
-            //AddressContext adr2 = new(arr2[0][0], arr2[0][1], arr2[0][2], arr2[0][3]);
-            //adr2.SubnetMask = SubnetMask2;
-            //adr2.NetworkAddress = Formater.GetNetworkAddress(arr2[0], arr2[1]);
-            //adr2.BroadcastAddress = Formater.GetBroadCastAddress(arr2[0], arr2[1]);
-            //adr2.TotalPossibleHosts = Formater.GetSubnettingHostAmount(arr2[0], arr2[1]);
-
-            //Console.WriteLine(adr.ToString());
-
-            //Console.WriteLine("\n" + adr2.ToString());
-
         }
     }
 }
